5ef71043252a66a2004802cab956aa4a8aa39abb
Huynh Anh Kiet <akiethuynh0904@gmail.com>
2025-12-16 01:14:10 +0700

    fix link pdfx6

--- app/src/components/About/components/TitleAbout/TitleAbout.tsx
+++ app/src/components/About/components/TitleAbout/TitleAbout.tsx
@@ -2,14 +2,16 @@
 import { Wrapper, Title, ButtonResume} from "./StylesTitleAbout";
 
 function TitleAbout() {
+  const base = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
   return (
         <Wrapper>
           <Title>Hi, I&lsquo;m Huynh Anh Kiet, a passionate fresher developer with a keen interest in ReactJS, NextJS, TypeScript. I am dedicated to honing my skills and continuously improving in the world of software development. My ultimate goal is to evolve into a proficient software engineer who can contribute effectively to innovative projects.</Title>
           <ButtonResume 
             as="a"
-            href={`${process.env.NEXT_PUBLIC_BASE_PATH}/Huynh-Anh-Kiet-CV.pdf`}
-            target="_blank">
+            href={`${base}/Huynh-Anh-Kiet-CV.pdf`}
+            target="_blank"
+            rel="noopener noreferrer">
               my resume
           </ButtonResume>
         </Wrapper>
